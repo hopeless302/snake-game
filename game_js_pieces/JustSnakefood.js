39a3597dfc37c1a11ba4f3dfe5513396f80c972b
Talha Anwar <mtalhaanwar359@gmail.com>
2024-03-22 22:51:35 +0500

    First commit of snake game

---
+++ game_js_pieces/JustSnakefood.js
@@ -0,0 +1,37 @@
+// This is js code in which we just make the food of the sanke
+
+// Define the HTML elements
+const board = document.getElementById("game-board");
+
+// Define game variables
+let snake = [{ x: 10, y: 10 }];
+
+//Draw game map, snake, food
+
+function draw() {
+  board.innerHTML = "";
+  drawSnake();
+}
+
+// Draw Snake
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+    setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
+}
+
+// Create a snake or food div
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className; // What this does is to make a div with the name of snake
+  return element;
+}
+
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
+
+draw();
